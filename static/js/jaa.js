0a1b1426b74d672c1d9961e9f79f12972734f0f9
kurmananda <edu.kurmananda@gmail.com>
2024-10-16 20:56:16 +0530

    more files

---
+++ static/js/jaa.js
@@ -0,0 +1,56 @@
+// URL of the API endpoint
+const url = 'http://172.20.42.70:5000';
+
+// Fetch data from the Django server
+fetch(url+'/candidates')
+  .then(response => response.json())
+  .then(data => {
+    console.log('GET Response:', data);
+    
+    document.getElementById('Welcome').textContent=data;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+  
+
+
+function re()
+{
+    if(document.getElementById('check1').checked && document.getElementById('check2').checked){
+        var sc = document.getElementById('inpu').value;
+        const postData = {
+            'Voter': sc,
+            'Position': "Chief Web Developer",
+            'Candidate': 'Aditya MishraG'
+          };
+        
+          
+  // Data to be sent in the POST request
+  
+  // Options for the fetch request
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(postData),
+  };
+
+  
+
+    // Make the POST request
+    fetch(url + '/vote', options)
+    .then(response => response.json())
+    .then(data => {
+        console.log('POST Response:', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+          
+    }
+
+    else 
+        alert('fill out every box')
+}
